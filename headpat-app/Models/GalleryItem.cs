688d7fa01bab3de3c017dc31ee9e21559bebcc6c
veve196 <veve196x@gmail.com>
2023-09-12 21:32:36 +0200

    ᓚᘏᗢ

--- headpat-app/Models/GalleryItem.cs
+++ headpat-app/Models/GalleryItem.cs
@@ -1,24 +1,13 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using static Microsoft.Maui.Controls.Internals.Profile;
-using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
-
-namespace HeadpatCommunity.Mobile.HeadpatApp.Models
+﻿namespace HeadpatCommunity.Mobile.HeadpatApp.Models
 {
     public class GalleryItem
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public bool IsNsfw { get; set; }
+        public bool IsNotNsfw => !IsNsfw; // Is Not Not Safe For Work :1lolol:
         public DateTime Created { get; set; }
         public string ImageUrl { get; set; }
-
-        public ImageSource Image => ImageSource.FromUri(new Uri(ImageUrl));
         public string ThumbnailImageUrl { get; set; }
     }
 }
