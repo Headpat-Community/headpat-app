49ed50c8ee713b8719d6a7dd3513d0f63ed3554f
Faye <dominic@stilma.net>
2024-03-18 21:36:05 +0100

    change to npm

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ export default function TabsLayout() {
       <Tabs.Screen
         name='index'
         options={{
-          title: 'Demo',
+          title: 'Gallery',
           tabBarIcon({ color, size }) {
             return <LayoutPanelLeft color={color} size={size} />;
           },
