5f03270f800650d55d209ccee22a3bb04ae3258c
Faye <dominic@stilma.net>
2024-05-24 23:13:14 +0200

    add plist files

--- app/friends/(tabs)/share.tsx
+++ app/friends/(tabs)/share.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { View } from 'react-native'
+import React from 'react'
+import { Button, StyleSheet, View } from 'react-native'
 import { H1 } from '~/components/ui/typography'
-import { Button } from '~/components/ui/button'
 import { Text } from '~/components/ui/text'
 import { database } from '~/lib/appwrite-client'
 import { useUser } from '~/components/contexts/UserContext'
@@ -9,66 +8,136 @@ import * as Location from 'expo-location'
 import * as BackgroundFetch from 'expo-background-fetch'
 import * as TaskManager from 'expo-task-manager'
 
+const BACKGROUND_FETCH_TASK = 'sharing-background-location'
 const LOCATION_TASK_NAME = 'background-location-task'
 
-TaskManager.defineTask(LOCATION_TASK_NAME, async () => {
-  console.log('test')
-  const { status } = await Location.requestForegroundPermissionsAsync()
+// 1. Define the task by providing a name and the function that should be executed
+// Note: This needs to be called in the global scope (e.g outside of your React components)
+TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+  // Request location permissions
+  let { status } = await Location.requestForegroundPermissionsAsync()
+  const { current }: any = useUser() // Move the hook here
+  console.log('current', current)
+
   if (status !== 'granted') {
     return BackgroundFetch.BackgroundFetchResult.Failed
   }
 
+  // Get current location
   const location = await Location.getCurrentPositionAsync({})
-  const { current }: any = useUser()
-  console.log('doing stuff')
+
+  // Make API calls to update or create location document
   try {
-    await database.updateDocument('hp_db', 'locations', current.userId, {
+    await database.updateDocument('hp_db', 'locations', current.$id, {
       long: location.coords.longitude,
       lat: location.coords.latitude,
     })
   } catch (error) {
     console.error('Error updating location', error)
-    await database.createDocument('hp_db', 'locations', current.userId, {
+    await database.createDocument('hp_db', 'locations', current.$id, {
       long: location.coords.longitude,
       lat: location.coords.latitude,
+      timeUntilEnd: new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString(),
     })
   }
 
   return BackgroundFetch.BackgroundFetchResult.NewData
 })
 
-export default function SharePage() {
-  const [sharing, setSharing] = useState<boolean>(false)
-  const [debug, setDebug] = useState<any>(null)
+TaskManager.defineTask(LOCATION_TASK_NAME, (locations) => {
+  console.log('Received new locations', locations)
+  // Your code to share the location goes here
+})
 
-  const startSharingLocation = async () => {
-    setSharing(true)
-    const data = await BackgroundFetch.registerTaskAsync(LOCATION_TASK_NAME, {
-      minimumInterval: 10, // seconds,
-    })
-    console.log(data)
-    setDebug(data)
+async function registerBackgroundFetchAsync() {
+  await BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
+    minimumInterval: 60 * 15, // 15 minutes
+    stopOnTerminate: false, // android only,
+    startOnBoot: true, // android only
+  })
+
+  let { status } = await Location.requestBackgroundPermissionsAsync()
+  if (status !== 'granted') {
+    console.error('Permission to access location was denied')
+    return
   }
 
-  const stopSharingLocation = async () => {
-    setSharing(false)
-    await BackgroundFetch.unregisterTaskAsync(LOCATION_TASK_NAME)
+  await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+    accuracy: Location.Accuracy.High,
+  })
+}
+
+async function unregisterBackgroundFetchAsync() {
+  await BackgroundFetch.unregisterTaskAsync(BACKGROUND_FETCH_TASK)
+  await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME)
+}
+
+export default function ShareLocationView() {
+  const [isRegistered, setIsRegistered] = React.useState(false)
+  const [status, setStatus] = React.useState(null)
+
+  React.useEffect(() => {
+    checkStatusAsync().then()
+  }, [])
+
+  const checkStatusAsync = async () => {
+    const status = await BackgroundFetch.getStatusAsync()
+    const isRegistered = await TaskManager.isTaskRegisteredAsync(
+      LOCATION_TASK_NAME
+    )
+    setStatus(status)
+    setIsRegistered(isRegistered)
+  }
+
+  const toggleFetchTask = async () => {
+    if (isRegistered) {
+      await unregisterBackgroundFetchAsync()
+    } else {
+      await registerBackgroundFetchAsync()
+    }
+
+    checkStatusAsync()
   }
 
   return (
-    <View className={'flex-1 justify-center items-center h-full'}>
-      <View className={'p-4 native:pb-24 max-w-md gap-6'}>
-        <View className={'gap-1'}>
-          <H1 className={'text-foreground text-center'}>Share my location</H1>
-          <Button
-            className={'text-base text-center'}
-            onPress={sharing ? stopSharingLocation : startSharingLocation}
-          >
-            <Text>{sharing ? 'Stop Sharing' : 'Start Sharing'}</Text>
-            <Text>{debug}</Text>
-          </Button>
-        </View>
+    <View style={styles.screen}>
+      <View style={styles.textContainer}>
+        <Text>
+          Background fetch status:{' '}
+          <Text style={styles.boldText}>
+            {status && BackgroundFetch.BackgroundFetchStatus[status]}
+          </Text>
+        </Text>
+        <Text>
+          Background fetch task name:{' '}
+          <Text style={styles.boldText}>
+            {isRegistered ? BACKGROUND_FETCH_TASK : 'Not registered yet!'}
+          </Text>
+        </Text>
       </View>
+      <View style={styles.textContainer}></View>
+      <Button
+        title={
+          isRegistered
+            ? 'Unregister BackgroundFetch task'
+            : 'Register BackgroundFetch task'
+        }
+        onPress={toggleFetchTask}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textContainer: {
+    margin: 10,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+})
