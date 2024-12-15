64594918060a3bdbf3b277cfa47fde257f25d69a
Faye <faye@fayevr.dev>
2024-12-15 19:20:38 +0100

    use multiple files for translations

--- components/system/i18n.ts
+++ components/system/i18n.ts
@@ -1,4 +1,16 @@
 import { I18n } from 'i18n-js'
-import { translations } from '~/components/data/localization'
+import en from '~/components/i18n/translations/en.json'
+import de from '~/components/i18n/translations/de.json'
+import nl from '~/components/i18n/translations/nl.json'
 
-export const i18n = new I18n(translations)
+export const i18n = new I18n({
+  translations: {
+    en,
+    de,
+    nl,
+  },
+  options: {
+    enableFallback: true,
+    defaultLocale: 'en',
+  }
+})
