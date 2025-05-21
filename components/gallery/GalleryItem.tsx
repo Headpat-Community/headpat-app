bfaf5bae3dadfa788cca9944731aa719c2f3249e
Faye <faye@fayevr.dev>
2025-05-21 22:02:46 +0200

    fix: gallery item routing

--- components/gallery/GalleryItem.tsx
+++ components/gallery/GalleryItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Dimensions, TouchableWithoutFeedback, View } from 'react-native'
 import { Image } from 'expo-image'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { Badge } from '~/components/ui/badge'
 import { Text } from '~/components/ui/text'
 import { ImageFormat } from 'react-native-appwrite'
@@ -33,62 +33,61 @@ const GalleryItem = React.memo(
     const isHidden: boolean = imagePrefs[image.$id]?.isHidden
 
     return (
-      <TouchableWithoutFeedback>
-        <Link
-          href={{
+      <TouchableWithoutFeedback
+        onPress={() => {
+          router.push({
             pathname: '/gallery/(stacks)/[galleryId]',
             params: { galleryId: image.$id },
+          })
+        }}
+      >
+        <View
+          style={{
+            position: 'relative',
+            width: widthColumns,
+            height: 200,
+            marginBottom: 10,
           }}
-          asChild
         >
-          <View
-            style={{
-              position: 'relative',
-              width: widthColumns,
-              height: 200,
-              marginBottom: 10,
-            }}
-          >
-            {isHidden ? (
-              <View>
-                <Skeleton className={'w-full h-full'}>
-                  <Badge className={'w-full'}>
-                    <Text>Image is hidden</Text>
-                  </Badge>
-                </Skeleton>
-              </View>
-            ) : (
-              <>
-                <Image
-                  source={
-                    image.mimeType.includes('video')
-                      ? { uri: thumbnail }
-                      : {
-                          uri: getGalleryUrl(image.$id, imageFormat),
-                        }
-                  }
-                  alt={image.name}
-                  style={{ width: '100%', height: '100%' }}
-                  contentFit={'contain'}
-                />
-                {image.mimeType.includes('gif') && (
-                  <Badge
-                    className={'absolute border-2 bg-secondary border-primary'}
-                  >
-                    <Text className={'text-primary'}>GIF</Text>
-                  </Badge>
-                )}
-                {image.mimeType.includes('video') && (
-                  <Badge
-                    className={'absolute border-2 bg-secondary border-primary'}
-                  >
-                    <Text className={'text-primary'}>Video</Text>
-                  </Badge>
-                )}
-              </>
-            )}
-          </View>
-        </Link>
+          {isHidden ? (
+            <View>
+              <Skeleton className={'w-full h-full'}>
+                <Badge className={'w-full'}>
+                  <Text>Image is hidden</Text>
+                </Badge>
+              </Skeleton>
+            </View>
+          ) : (
+            <>
+              <Image
+                source={
+                  image.mimeType.includes('video')
+                    ? { uri: thumbnail }
+                    : {
+                        uri: getGalleryUrl(image.$id, imageFormat),
+                      }
+                }
+                alt={image.name}
+                style={{ width: '100%', height: '100%' }}
+                contentFit={'contain'}
+              />
+              {image.mimeType.includes('gif') && (
+                <Badge
+                  className={'absolute border-2 bg-secondary border-primary'}
+                >
+                  <Text className={'text-primary'}>GIF</Text>
+                </Badge>
+              )}
+              {image.mimeType.includes('video') && (
+                <Badge
+                  className={'absolute border-2 bg-secondary border-primary'}
+                >
+                  <Text className={'text-primary'}>Video</Text>
+                </Badge>
+              )}
+            </>
+          )}
+        </View>
       </TouchableWithoutFeedback>
     )
   }
