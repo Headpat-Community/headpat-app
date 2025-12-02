e1004da3b01a9a2b323660ce0d5aa7fd1265bddc
Faye <faye@fayevr.dev>
2025-12-02 21:45:15 +0100

    fix: loadDictionary

--- src/loadDictionary.ts
+++ src/loadDictionary.ts
@@ -1,12 +1,13 @@
-export default async function loadDictionary(locale: string) {
-  switch (locale) {
-    case 'en':
-      return (await import('~/components/i18n/translations/en.json')).default
-    case 'de':
-      return (await import('~/components/i18n/translations/de.json')).default
-    case 'nl':
-      return (await import('~/components/i18n/translations/nl.json')).default
-    default:
-      return (await import('~/components/i18n/translations/en.json')).default
-  }
+import de from '~/components/i18n/translations/de.json'
+import en from '~/components/i18n/translations/en.json'
+import nl from '~/components/i18n/translations/nl.json'
+
+const dictionaries = {
+  en,
+  de,
+  nl,
+} as const
+
+export async function loadDictionary(locale: string) {
+  return Promise.resolve(dictionaries[locale as keyof typeof dictionaries])
 }
