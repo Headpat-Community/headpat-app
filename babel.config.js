affa94756bd773408c3d5bb8573fbd782c60d209
Ernest McCarter <ernest@generaltranslation.com>
2025-12-02 11:56:45 -0800

    chore: setup gt i18n

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+import gtPlugin from 'gt-react-native/plugin'
+import { createRequire } from 'module';
+import gtConfig from './gt.config.json' with { type: 'json' };
+
+const require = createRequire(import.meta.url);
+
 export default function (api) {
   api.cache(true)
   return {
@@ -5,6 +11,16 @@ export default function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: ["react-native-worklets/plugin"],
+    plugins: [
+      "react-native-worklets/plugin",
+      [
+        gtPlugin,
+        {
+          locales: [gtConfig.defaultLocale, ...gtConfig.locales],
+          entryPointFilePath: require.resolve('expo-router/entry'),
+        },
+      ],
+
+    ],
   }
 }
